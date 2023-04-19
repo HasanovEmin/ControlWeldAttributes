e168caaaa63f4bd938781e2c53ff40a88001b005
Emin Hasanov <emin.hasanov.90@outlook.com>
2023-04-19 19:55:21 +0400

    Add project files.

---
+++ ControlWeldAttributes/Controller.cs
@@ -0,0 +1,73 @@
+﻿using Aveva.Core.Database;
+using System;
+namespace ControlWeldAttributes
+{
+    public static class Controller
+    {
+        public static DbAttribute WeldPositionFlag { get; set; }
+        public static DbAttribute WeldPosition { get; set; }
+        public static void Run()
+        {
+            WeldPositionFlag = DbAttribute.GetDbAttribute(":WeldPositionFlag");
+            WeldPosition = DbAttribute.GetDbAttribute(":WeldPosition");
+            if (WeldPositionFlag == null || WeldPosition == null)
+            {
+                return;
+            }
+
+            DbPostElementChange.AddPostAttributeChange(DbAttributeInstance.POS, new DbPostElementChange.PostAttributeChangeDelegate(PositionChanged));
+            DbPostElementChange.AddPostAttributeChange(DbAttribute.GetDbAttribute(":WeldPositionFlag"), new DbPostElementChange.PostAttributeChangeDelegate(WeldPositionFlagChanged));
+
+        }
+        private static void WeldPositionFlagChanged(DbElement ele, DbAttribute att)
+        {
+            if (ele.ElementType != DbElementTypeInstance.WELD)
+                return;
+
+            var flag = ele.GetBool(att);
+            try
+            {
+                if (flag == true)
+                {
+                    var position = ele.GetAsString(DbAttributeInstance.POS);
+                    ele.SetAttribute(WeldPosition, position);
+                }
+                else
+                {
+                    ele.SetAttribute(WeldPosition, "");
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+
+        }
+
+        private static void PositionChanged(DbElement ele, DbAttribute att)
+        {
+            if (ele.ElementType != DbElementTypeInstance.WELD)
+                return;
+
+            var flag = ele.GetBool(WeldPositionFlag);
+            try
+            {
+                if (flag == true)
+                {
+                    var position = ele.GetAsString(att);
+                    ele.SetAttribute(WeldPosition, position);
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+
+
+        }
+
+
+
+
+    }
+}
---
+++ ControlWeldAttributes/StartPoint.cs
@@ -0,0 +1,25 @@
+﻿using Aveva.ApplicationFramework;
+
+
+namespace ControlWeldAttributes
+{
+    public class StartPoint : IAddin
+    {
+        public string Name => "ControlWeldAttributes";
+
+        public string Description => "Control attributes for Weld when it creating and modificating.";
+
+        
+        public void Start(ServiceManager serviceManager)
+        {
+            Controller.Run();
+           
+        }
+
+      
+
+        public void Stop() { }
+
+
+    }
+}
